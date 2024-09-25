a1ff716c2f89881d69966eee2003d98c93d32788
Thomas Grandjean <thomasgrandjean45@gmail.com>
2024-09-25 14:21:40 +0200

    modif clients.html => .php

--- clients.php
+++ clients.php
@@ -1,27 +1,5 @@
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Profil</title>
-    <meta name="clients" content="Profil">
-    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="styles\styles.css" type="text/css">
-</head>
-
-<body>
-
-    <!-- ===header=== -->
-
-    <header class="header">
-
-    </header>
-
-    <!-- ===fin header=== -->
-
+<?php include 'layout.php'; ?>
+<?php include 'header.php'; ?>
 
     <!-- ===main=== -->
       
@@ -139,12 +117,10 @@
 
     <!-- ===footer=== -->
 
-    <footer class="footer">
+    <?php include 'footer.php'; ?>
 
-    </footer>
 
     <!-- ===fin footer=== -->
-</body>
 
 
     <!-- ===licences=== 
