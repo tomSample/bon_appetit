1b1e5d8826999f017e806f9237dcad2cbe9fe718
Yannick <pro@ygras.fr>
2024-10-10 11:29:50 +0200

    Création branche

---
+++ create.php
@@ -0,0 +1,29 @@
+<?php include 'layout.php'; ?>
+<?php include 'header.php'; ?>
+
+<head>
+        <link rel="stylesheet" href="styles\create.css" type="text/css">
+    </head>
+
+<body>
+
+    <!-- ===main=== -->
+
+    <section id="management" class="fields">
+
+
+
+
+    
+    </section>
+
+    <!-- ===fin main=== -->
+
+
+    <!-- ===footer=== -->
+
+    <?php include 'footer.php'; ?>
+
+    <!-- ===fin footer=== -->
+     
+</body>
