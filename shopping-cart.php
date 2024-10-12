20dedd11a1faa8a640551c7f05692c84d74c601a
Thomas Grandjean <thomasgrandjean45@gmail.com>
2024-10-12 15:36:35 +0200

    50% done on shopping cart structure

--- shopping-cart.php
+++ shopping-cart.php
@@ -1,6 +1,60 @@
 <?php include 'layout.php'; ?>
 <?php include 'header.php'; ?>
 <?php include 'items.php'; ?>
+<link rel="stylesheet" href="styles\shopping-cart.css" type="text/css">
+
+<section id="shopping-cart-body">
+
+<div id="shopping-cart-container-left">
+    <div id="shopping-cart-content">
+        <div id="shopping-cart-content-title">
+            <h2>Détails livraison</h2>
+        </div>
+        <div id="shopping-cart-content-box">
+            <div>
+                <h3>À emporter</h3>
+            </div>
+            <div id="shopping-cart-button-box">
+                <button id="take-away-button" class="cart-modify-button">Modifier</button>
+            </div>
+        </div>
+        <div>
+            <div id="shopping-cart-content-box">
+                <div>
+                    <h3>À domicile</h3>
+                </div>
+                <div id="shopping-cart-button-box">
+                    <button id="delivery-button" class="cart-modify-button">Modifier</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="shopping-cart-content">
+        <div id="shopping-cart-content-title">
+            <h2>Moyens de paiement</h2>
+        </div>
+        <div id="shopping-cart-content-box">
+            <div>
+                <h3>Visa 4971 8545 ...</h3>
+            </div>
+            <div id="shopping-cart-button-box">
+                <button id="take-away-button" class="cart-modify-button">Modifier</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div id="shopping-cart-container-right" >
+    <div id="shopping-cart-content-title">
+
+    </div>
+
+    <div id="shopping-cart-content-title">
+        
+        </div>
+</div>
 
 
 
@@ -14,4 +68,8 @@
 
 
 
+
+
+
+</section>
 <?php include 'footer.php'; ?>
