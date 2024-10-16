9f1ef34f7f6dd4a3ff4bdbed7b013fcc2c5b3ba1
MathisFossard <mathisfossard22@gmail.com>
2024-10-16 13:18:10 +0200

    inscription-restaurant-css-update

---
+++ inscription-restaurant.php
@@ -0,0 +1,63 @@
+<?php include 'layout.php'; ?>
+<?php include 'header.php'; ?>
+
+
+<!-- ===hero banner=== -->
+
+    <section class="hero-banner">
+            <div class="inscriptionrestaurant-container">
+                <div class="inscription-title">
+                        <h1>Inscription restaurant</h1>
+                </div>
+                <div class="inscription-undertitle">
+                        <p>Indiquez vos informations</p>
+                </div>
+                    <section class="container-inscrirestau">
+                        <section class="section-inscrirestau1">
+                            <div class="questionnaire">
+                                <input id="hero-search-bar" type="search" placeholder="Entrez votre nom d'utilisateur">
+                            </div>
+                            <div class="questionnaire">
+                                <input id="hero-search-bar" type="search" placeholder="Entrez votre mot de passe">
+                            </div>
+                            <div class="questionnaire">
+                                <input id="hero-search-bar" type="search" placeholder="Confirmez votre mot de passe">
+                            </div>
+                            <div class="questionnaire">
+                            <input id="hero-search-bar" type="search" placeholder="Entrez votre numéro de téléphone portable">
+                            </div>
+                        </section>
+                            <section class="section-inscrirestau2">
+                            <div class="questionnaire">
+                                <input id="hero-search-bar" type="search" placeholder="Entrez le nom de votre établissement">
+                            </div>
+                            <div class="questionnaire">
+                                <input id="hero-search-bar" type="search" placeholder="Entrez l'adresse de votre établissement">
+                            </div>
+                            <div class="questionnaire">
+                                <input id="hero-search-bar" type="search" placeholder="Entrez le type d'entreprise">
+                            </div>
+                            <div class="questionnaire">
+                                <input id="hero-search-bar" type="search" placeholder="Entrez le nom de la marque">
+                            </div>
+                            </section>
+                        </section>
+                    <div class="inscription-confirmation-button-restauinscri">
+                    <div class="inscription-confirmation-button">
+                        <button class="submit-button submit-button-inscription" type="submit">S'inscrire</button>
+                    </div>
+                    </div>
+                </s>
+            </div>
+        </section>
+    </section>
+
+<!-- ===fin hero-banner=== -->
+
+
+<!-- ===footer=== -->
+<?php include 'footer.php'; ?>
+<!-- ===fin footer=== -->
+
+    </body>
+</html>
