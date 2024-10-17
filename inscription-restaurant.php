0c4c7f93bab0f2b6469b83935f636f13fc9b6aba
MathisFossard <mathisfossard22@gmail.com>
2024-10-17 12:45:50 +0200

    inscription_restaurant_verif_before_merge

--- inscription-restaurant.php
+++ inscription-restaurant.php
@@ -18,38 +18,54 @@
                     <section class="container-inscrirestau">
                         <section class="section-inscrirestau1">
                             <div class="questionnaire">
+                            <div class="questionnaire-inscrirestau">
                                 <input id="hero-search-bar" type="search" placeholder="Entrez votre nom d'utilisateur">
                             </div>
+                            </div>
                             <div class="questionnaire">
+                            <div class="questionnaire-inscrirestau">
                                 <input id="hero-search-bar" type="search" placeholder="Entrez votre mot de passe">
                             </div>
-                            <div class="questionnaire">
-                                <input id="hero-search-bar" type="search" placeholder="Confirmez votre mot de passe">
                             </div>
                             <div class="questionnaire">
-                            <input id="hero-search-bar" type="search" placeholder="Entrez votre numéro de téléphone portable">
+                            <div class="questionnaire-inscrirestau">
+                                <input id="hero-search-bar" type="search" div placeholder="Confirmez votre mot de passe">
+                            </div>
+                            </div>
+                            <div class="questionnaire">
+                            <div class="questionnaire-inscrirestau">
+                            <input id="hero-search-bar" type="search" placeholder="Entrez votre numéro de mobile">
+                            </div>
                             </div>
                         </section>
                             <section class="section-inscrirestau2">
                             <div class="questionnaire">
-                                <input id="hero-search-bar" type="search" placeholder="Entrez le nom de votre établissement">
+                            <div class="questionnaire-inscrirestau">
+                                <input id="hero-search-bar" type="search" placeholder="Entrez le nom de l'établissement">
+                            </div>
                             </div>
                             <div class="questionnaire">
-                                <input id="hero-search-bar" type="search" placeholder="Entrez l'adresse de votre établissement">
+                            <div class="questionnaire-inscrirestau">
+                                <input id="hero-search-bar" type="search" placeholder="Entrez l'adresse de l'établissement">
+                            </div>
                             </div>
                             <div class="questionnaire">
+                            <div class="questionnaire-inscrirestau">
                                 <input id="hero-search-bar" type="search" placeholder="Entrez le type d'entreprise">
                             </div>
+                            </div>
                             <div class="questionnaire">
+                            <div class="questionnaire-inscrirestau">
                                 <input id="hero-search-bar" type="search" placeholder="Entrez le nom de la marque">
                             </div>
+                            </div>
                             </section>
-                        </section>
-                    <div class="inscription-confirmation-button-restauinscri">
+                            <div class="inscription-confirmation-button-restauinscri">
                     <div class="inscription-confirmation-button">
                         <button class="submit-button submit-button-inscription" type="submit">S'inscrire</button>
                     </div>
                     </div>
+                        </section>
                 </s>
             </div>
         </section>
