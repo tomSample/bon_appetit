bddca4932a6158082719efd66347bcb52f3fa886
Yannick <pro@ygras.fr>
2024-10-19 18:20:22 +0200

    Clients are dynamics

--- clients.php
+++ clients.php
@@ -16,7 +16,7 @@
         </div>
         <div class="container" id="identity">
             <div class="subtitle">
-                <h2>Identité</h2><button class="clients__buttons__yellow"><img class="identity__icon" src="./img/icon/editing.png"></button>
+                <h2>Identité</h2><button class="clients__buttons__yellow witcher"><img class="identity__icon" src="./img/icon/editing.png"></button>
             </div>
             <div class="grid">
                 <div class="together">
@@ -24,96 +24,125 @@
                         <div class="grid__item__left">
                             <p>Nom, Prénom</p>
                         </div>
-                        <div>
+                        <div class="changed" id="name">
                             <p>John Doe</p>
+                        </div>
+                        <div class="change hide" id="name2">
+                            <input id="change__name" type="change__type" class="change__type" placeholder=" John Doe">
                         </div>
                     </div>
                     <div class="clients__alone">
                         <div class="grid__item__left">
-                            <p>Adresse de livraison</p>
+                            <p>Livraison</p>
                         </div>
-                        <div>
+                        <div class="changed" id="adress">
                             <p>56 rue de Brest</p>
+                        </div>
+                        <div class="change hide" id="adress2">
+                            <input id="change__adress" type="change__type" class="change__type" placeholder=" 56 rue de Brest">
                         </div>
                     </div>
                     <div class="clients__alone">
                         <div class="grid__item__left">
                             <p>Code postal</p>
                         </div>
-                        <div>
+                        <div class="changed" id="postal__code">
                             <p>22300</p>
+                        </div>
+                        <div class="change hide" id="postal__code2">
+                            <input id="change__city" type="change__type" class="change__type" placeholder=" 22300">
                         </div>
                     </div>
                     <div class="clients__alone">
                         <div class="grid__item__left">
                             <p>Ville</p>
                         </div>
-                        <div>
+                        <div class="changed" id="city">
                             <p>Lannion</p>
+                        </div>
+                        <div class="change hide" id="city2">
+                            <input id="change__city" type="change__type" class="change__type" placeholder=" Lannion">
                         </div>
                     </div>
                     <div class="clients__alone">
                         <div class="grid__item__left">
                             <p>E-mail</p>
                         </div>
-                        <div>
+                        <div class="changed" id="email">
                             <p>monadresse@email.fr</p>
+                        </div>
+                        <div class="change hide" id="email2">
+                            <input id="change__email" type="change__type" class="change__type" placeholder=" monadresse@email.fr">
                         </div>
                     </div>
                     <div class="clients__alone">
                         <div class="grid__item__left">
                             <p>Téléphone</p>
                         </div>
-                        <div>
+                        <div class="changed">
                             <p>06 78 98 65 15</p>
                         </div>
+                        <div class="change hide" id="phone2">
+                            <input id="change__phone" type="change__type" class="change__type" placeholder=" 06 78 98 65 15">
+                        </div>
+                    </div>
+                    <div class="change hide">
+                        <button class="submit-button" id="identity__button" type="submit">Enregistrer</button>
                     </div>
                 </div>
                 <div class="together">
                     <div class="clients__alone">
                         <div class="grid__item__left">
-                            <p>Adresse de facturation</p>
+                            <p>Facturation</p>
                         </div>
-                        <div class="clients__alone">
-                            <p>32 rue de Paris</p><button class="clients__buttons__yellow"><img class="clients__icon__intern" src="./img/icon/add.png"></button>
+                        <div class="clients__alone" id="hide__me">
+                            <label for="invoice__select"></label>
+                                <select name="invoice" id="invoice__select">  
+                                    <option value="invoice1" class="option">32 rue de Paris, Lannion</option>
+                                    <option value="invoice2" class="option">22 rue de Paris, Morlaix</option>
+                                    <option value="invoice3" class="option">12 rue de Paris, Plougannou</option>
+                                </select><button class="clients__buttons__yellow addproof"><img class="clients__icon__intern" src="./img/icon/add.png"></button><button class="clients__buttons__yellow proof"><img class="clients__icon__intern" src="./img/icon/delete.png"></button>
                         </div>
-                    </div>
-                    <div class="clients__alone">
-                        <div class="grid__item__left">
-                            <p>Code postal</p>
-                        </div>
-                        <div>
-                            <p>22300</p>
-                        </div>
-                    </div>
-                    <div class="clients__alone">
-                        <div class="grid__item__left">
-                            <p>Ville</p>
-                        </div>
-                        <div>
-                            <p>Lannion</p>
-                        </div> 
-                    </div>
-                </div>
-                <div class="together">
-                    <div class="clients__alone">
-                        <div class="grid__item__left">
-                            <p>Mode de paiement</p>
-                        </div>
-                        <div class="clients__alone">
-                            <p>Carte bleue</p><button class="clients__buttons__yellow"><img class="clients__icon__intern" src="./img/icon/arrow.png"></button>
+                        <div class="hide" id="newinvoice">
+                            <div class="resto-id-specs" id="invoice__newp">
+                                    <div class="close__invoice">
+                                        <input id="invoice__name" type="add__invoice" class="add__invoice" placeholder=" Adresse"><button class="clients__buttons__yellow proof__off"><img class="clients__icon__intern" src="./img/icon/close.png"></button>
+                                    </div>
+                                    <input id="invoice__number" type="add__invoice" class="add__invoice" placeholder=" Code postal">
+                                    <input id="invoice__number" type="add__invoice" class="add__invoice" placeholder=" Ville">
+                                    <button class="submit-button" id="invoice__button" type="submit">Enregistrer</button>
+                            </div>
                         </div>
                     </div>
                     <div class="clients__alone">
                         <div class="grid__item__left">
-                            <p>Derniers numéros<br></p>
-                        </div> 
-                        <div>
-                            <p>2659</p>
+                            <p>Paiement</p>
+                        </div>
+                        <div class="clients__alone" id="find__me">
+                            <label for="pay__select"></label>
+                                <select name="pay" id="pay__select">  
+                                    <option value="pay1" class="option">Carte bleue #2659</option>
+                                    <option value="pay2" class="option">Carte bleue #3274</option>
+                                    <option value="pay3" class="option">Carte restaurant</option>
+                                </select><button class="clients__buttons__yellow turn"><img class="clients__icon__intern" src="./img/icon/add.png"></button><button class="clients__buttons__yellow turn"><img class="clients__icon__intern" src="./img/icon/delete.png"></button>
+                        </div>
+                        <div class="hide" id="plus">
+                            <div class="resto-id-specs" id="clients__newp">
+                                <div class="selector">
+                                    <label for="type__select"></label>
+                                        <select name="type" id="type__select">  
+                                            <option value="type1" class="option">Carte bleue</option>
+                                            <option value="type2" class="option">Carte restaurant</option>
+                                            <option value="type2" class="option">Autre carte</option>
+                                        </select><button class="clients__buttons__yellow turn__off"><img class="clients__icon__intern" src="./img/icon/close.png"></button>
+                                </div>
+                                    <input id="add__name" type="add__type" class="add__type" placeholder=" Titulaire">
+                                    <input id="add__number" type="add__type" class="add__type" placeholder=" Numéro">
+                                    <input id="add__number" type="add__type" class="add__type" placeholder=" Expire le mm/aa">
+                                    <button class="submit-button" id="pay__button" type="submit">Enregistrer</button>
+                            </div>
                         </div>
                     </div>
-                        <!--<div class=clients__pay>
-                        </div>--> 
                 </div>
             </div>
         </div>
@@ -141,11 +170,21 @@
 
         <div class="container" id="love">
             <div class="subtitle">
-                <h2>Restaurant préféré</h2><button class="clients__buttons__yellow"><img class="identity__icon2" src="./img/icon/editing.png"></button>
+                <h2>Restaurant favori</h2><button class="clients__buttons__yellow whois"><img class="identity__icon2" src="./img/icon/editing.png"></button>
             </div>
             <div id="loved">
                 <p>Aux plaisirs gustatifs                    
                 </p>
+            </div>
+            <div class="clients__favorite hide">
+                <label for="favorite__select"></label>
+                    <select name="favorite" id="favorite__select">  
+                        <option value="" class="option">Choisir son favori</option>
+                        <option value="name1" class="option">Aux plaisirs gustatifs</option>
+                        <option value="name2" class="option">Mangez-moi</option>
+                        <option value="name2" class="option">Le 7e sens</option>
+                    </select>
+                <button class="submit-button" id="favorite__button" type="submit">Enregistrer</button>
             </div>
         </div>
 
@@ -203,7 +242,8 @@
     <!-- ===licences=== 
     <a href="https://www.flaticon.com/free-icons/download" title="download icons">Download icons created by joalfa - Flaticon</a>
     <a href="https://www.flaticon.com/free-icons/modify" title="modify icons">Modify icons created by Freepik - Flaticon</a>
-    <a href="https://www.flaticon.com/free-icons/plus" title="plus icons">Plus icons created by dmitri13 - Flaticon</a> 
+    <a href="https://www.flaticon.com/free-icons/plus" title="plus icons">Plus icons created by dmitri13 - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/subtract" title="subtract icons">Subtract icons created by Xinh Studio - Flaticon</a> 
     <a href="https://www.flaticon.com/free-icons/user-interface" title="user interface icons">User interface icons created by QudaDesign - Flaticon</a>
     <a href="https://www.canva.com/fr_fr/magique/" title="delete picture">Autre image générée par média magique de Canva</a> -->
 
--- js/clients.js
+++ js/clients.js
@@ -1,3 +1,103 @@
+/* Modifier les informations personnelles */
+
+function changeMe() {
+    const btn = document.querySelector('.witcher');
+    const links = document.querySelectorAll('.changed');
+    const others = document.querySelectorAll('.change');
+
+    btn.addEventListener('click', () => {
+        btn.classList.add('hide');
+        links.forEach(link => {
+            link.classList.add('hide');
+        });
+        others.forEach(other => {
+            other.classList.remove('hide');
+        });
+})
+};
+
+changeMe();
+
+
+/* Ajouter adresse de facturation */
+
+function invoiceMe() {
+    const btn = document.querySelector('.addproof');
+    const link = document.querySelector('#newinvoice');
+
+    btn.addEventListener('click', () => {
+        btn.parentNode.classList.add('hide');
+        link.classList.remove('hide');
+    });
+}
+
+invoiceMe();
+
+
+/* Cacher adresse de facturation */
+
+function hideMe() {
+    const btn = document.querySelector('.proof__off');
+    const link = document.querySelector('#newinvoice');
+    const renew = document.querySelector('#hide__me');
+
+    btn.addEventListener('click', () => {
+        renew.classList.remove('hide');
+        link.classList.add('hide');
+    });
+}
+
+hideMe();
+
+
+/* Ajouter moyen de paiement */
+
+function payMe() {
+    const btn = document.querySelector('.turn');
+    const link = document.querySelector('#plus');
+
+    btn.addEventListener('click', () => {
+        btn.parentNode.classList.add('hide');
+        link.classList.remove('hide');
+    });
+}
+
+payMe();
+
+
+/* Cacher moyen de paiement */
+
+function findMe() {
+    const btn = document.querySelector('.turn__off');
+    const link = document.querySelector('#plus');
+    const renew = document.querySelector('#find__me');
+
+    btn.addEventListener('click', () => {
+        renew.classList.remove('hide');
+        link.classList.add('hide');
+    });
+}
+
+findMe();
+
+
+/* Choisir son restaurant favori */
+
+function chooseMe() {
+    const btn = document.querySelector('.whois');
+    const link = document.querySelector('.clients__favorite');
+    const other = document.querySelector('#loved');
+
+    btn.addEventListener('click', () => {
+        btn.classList.add('hide');
+        link.classList.remove('hide');
+        other.classList.add('hide');
+    });
+}
+
+chooseMe();
+
+
 /* Affichage confirmation suppression du compte*/
 
 function deleteMe() {
