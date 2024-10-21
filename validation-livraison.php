cde1e7966d5f4c1da643ec286f92b049660559dc
MathisFossard <mathisfossard22@gmail.com>
2024-10-21 16:43:42 +0200

    validation_livraison_start

---
+++ validation-livraison.php
@@ -0,0 +1,22 @@
+<?php include 'layout.php'; ?>
+<?php include 'header.php'; ?>
+
+<!-- ===hero banner=== -->
+
+<section class="validlivraison-hero-banner">
+    <section class="validlivraison-sectioncontainer">
+        <section class="validlivraison-section1">
+            Vous avez une livraison     
+        </section>      
+    </section>
+</section>
+
+<!-- ===fin hero-banner=== -->
+
+
+<!-- ===footer=== -->
+<?php include 'footer.php'; ?>
+<!-- ===fin footer=== -->
+
+    </body>
+</html>
