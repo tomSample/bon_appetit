2c07fb4b7f0214992975b56c78745615e261e245
MathisFossard <mathisfossard22@gmail.com>
2024-10-25 14:13:23 +0200

    validation_livraison_finish

--- validation-livraison.php
+++ validation-livraison.php
@@ -4,11 +4,62 @@
 <!-- ===hero banner=== -->
 
 <section class="validlivraison-hero-banner">
-    <section class="validlivraison-sectioncontainer">
+    <section class="validlivraison-container1">
         <section class="validlivraison-section1">
             Vous avez une livraison     
-        </section>      
-    </section>
+        </section> 
+    </section>     
+    <section class="validlivraison-container2">
+        <section class="validlivraison-section2">
+            <div class="validlivraison-titresection1">
+                Informations du restaurant
+            </div>
+        <br>
+           Nom : Restaurant 1 
+        <br>
+           Addresse : Addresse 1 
+        <br>
+            Horaires : Ouvre à 11h - Ferme à 23h
+        <br>
+            Destination restaurant : À 750m
+        </section> 
+        <section class="validlivraison-section3">
+            <div class="validlivraison-titresection1">
+                Informations de la livraison
+            </div>
+        <br>
+           Poids : 15kg
+        <br>
+           Paquets : 2 paquets
+        <br>
+            Estimation temps souhaité : 25min
+        <br>
+            Destination point livraison : À 150m
+        </section> 
+    </section> 
+    <section class="validlivraison-container3">
+        <section class="validlivraison-section4">
+            <div class="validlivraison-containerbutton1">
+                            <div class="validlivraison-button1">
+                                <a href="information_livraison.php">Accepter la livraison</a>
+                            </div>
+            </div>
+        </section> 
+        <section class="validlivraison-section5">
+            <div class="validlivraison-containerbutton2">
+                            <div class="validlivraison-button2">
+                                <a href="#">Livraison suivante</a>
+                            </div>
+            </div>
+        </section> 
+        </section> 
+        <section class="validlivraison-section6">
+            <div class="validlivraison-containerbuttonmobile1">
+                            <div class="validlivraison-buttonmobile1">
+                                <a href="#">Livraison suivante</a>
+                            </div>
+            </div>
+        </section> 
 </section>
 
 <!-- ===fin hero-banner=== -->
