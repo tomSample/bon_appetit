86ba5ba7f50f9bc51900e56b560af1732aab03f7
Yannick <pro@ygras.fr>
2024-12-17 10:26:21 +0100

    Création historique

---
+++ historique.php
@@ -0,0 +1,53 @@
+<?php include 'layout.php'; ?>
+<?php include 'header.php'; ?>
+
+
+<body>
+
+    <!-- ===main=== -->
+      
+    <section id="history__page" class="fields">
+        <div class="container__title" id="history__title">
+            <div class="sectionHeaderHistory">
+                <h1>Historique</h1>
+            </div>
+        </div>
+
+    <!-- ===table=== -->
+
+        <div class="grid__item__view" id="hystory__data">
+            <div class="views__list">
+                <div class="data__shop">
+                    <div class="wrapTable">
+                        <div class="tableHeader">
+                            <div class="col1 iteman"><span>Date du jour</span></div>
+                            <div class="col2 iteman"><span>Nouveaux utilisateurs</span></div>
+                            <div class="col3 iteman"><span>Connexions</span></div>
+                        </div>
+                        <div class="tableBody">
+                            <div class="oneRow">
+                                <div class="iteman col1">
+                                    <div class="description">
+                                        <p class="date">17/12/2024</p>
+                                    </div>
+                                </div>
+                                <div class="iteman col2"><span>50</span></div>
+                                <div class="iteman col3"><span>350</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- ===fin main=== -->
+
+
+    <!-- ===footer=== -->
+
+    <?php include 'footer.php'; ?>
+
+    <!-- ===fin footer=== -->
+     
+</body>
